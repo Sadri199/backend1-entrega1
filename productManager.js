9bf68e3012791934b03d8b808df01f6dfec36252
Sadri199 <adrsa_15@hotmail.com>
2025-10-23 17:04:40 -0300

    avanzando con products manager

--- productManager.js
+++ productManager.js
@@ -1,6 +1,6 @@
 import fs from "fs/promises"
 
-class ProductManager {
+export class ProductManager {
 
     constructor(title, description, code, price, status, stock, category, thumbnail){
         this.id = Math.floor(Math.random() * (5000000 - 100) + 100) 
@@ -28,6 +28,7 @@ class ProductManager {
             console.log("products.json created or edited!")
         } catch (err){
             console.log("Something went wrong creating or updating the file: " + err)
+            return "Internal Server Error, please try again later."
         }
     }
 
@@ -35,23 +36,56 @@ class ProductManager {
         try{
             const getJson = await fs.readFile("./db/products.json", {encoding: "utf8"})
             console.log("Reading file...")
-            return getJson
+            const convertJson = JSON.parse(getJson)
+            return convertJson
         } catch (err){
             console.log("Something went wrong reading the file: " + err)
+            return "Internal Server Error, please try again later."
         }
-    
+    }
+
+    async getProductByID (id) {
+        try{
+            const getJson = await fs.readFile("./db/products.json", {encoding: "utf8"})
+            console.log("Reading file...")
+            const convertJson = JSON.parse(getJson)
+            const arrayGames = convertJson.games
+            const filteredGame = arrayGames. filter(game => game.id == id)
+            for (let i = 0; i < filteredGame.length; i++){
+                return filteredGame[i]
+            }
+        }catch (err){
+            console.log("Something went wrong while retrieving the file: " + err)
+            return "Internal Server Error, please try again later."
+        }
+    }
+
+    async updateProduct (id, data){
+        try{
+            const allProducts = getProduct()
+            const arrayGames = allProducts.games
+            const filteredGame = arrayGames. filter(game => game.id == id)
+            for (let i = 0; i < filteredGame.length; i++){
+                const currentGame = filteredGame[i]
+            }
+            //me quedé acá
+            
+        }catch(err){
+            console.log("Something went wrong while editing the file: " + err)
+            return "Internal Server Error, please try again later."
+        }
     }
 }
 
-const init = new ProductManager()
+// const init = new ProductManager()
 
-init.addProduct({
-    title: "Granddad 7",
-    description: "Flintstones",
-    code: "BTLG-1",
-    price: 500,
-    status: true,
-    stock: 5000000,
-    category: "Platformer",
-    thumbnail: ["https://i.kym-cdn.com/entries/icons/original/000/020/037/7_Grand_Dad_Banner.jpg"]
-})
+// init.addProduct({
+//     title: "Granddad 7",
+//     description: "Flintstones",
+//     code: "BTLG-1",
+//     price: 500,
+//     status: true,
+//     stock: 5000000,
+//     category: "Platformer",
+//     thumbnail: ["https://i.kym-cdn.com/entries/icons/original/000/020/037/7_Grand_Dad_Banner.jpg"]
+// })
--- server.js
+++ server.js
@@ -1,27 +1,75 @@
 import express from "express"
+import { ProductManager } from "./productManager.js"
 
 const app = express()
+const newManager = new ProductManager()
 
 const PORT = 8080
 
 app.use(express.json())
 
 app.get("/api/products", (req, res)=> {
-    console.log("Inside Get Products")
-    res.status(200)
-    res.send("My first Get")
+    const readJson = newManager.getProduct()
+    readJson
+        .then((data) =>{
+            res.status(200)
+            res.send(data)
+        })
+        .catch((data)=> {
+            res.status(500)
+            res.send(data)
+        })
 
 })
 
 app.post("/api/products",(req, res)=> {
-    console.log("Inside POST Products")
-    res.status(200)
-    res.send("My first Post")
+    try{
+        const newProduct = {
+            title:  req.body.title,
+            description: req.body.description,
+            code: req.body.code,
+            price: req.body.price,
+            status: req.body.status,
+            stock: req.body.stock,
+            category: req.body.category,
+            thumbnail: req.body.thumbnail
+        }
+        const createProduct = newManager.addProduct(newProduct)
+        createProduct
+            .then(()=> {
+                res.status(200)
+                res.json({status: "Succesful", message: "Product added correctly to our Database!"})
+            })
+            .catch((data)=> {
+                res.status(500)
+                res.send(data)
+            })
+    }catch(err){
+        console.log("Here is the errror: " + err)
+        res.status(400)
+        res.send("Something went wrong and we couldn't add that product to our Database.")
+    }
 
 })
 
-app.put("/api/products",(req, res)=> { //ID dinámico
-    console.log("Inside Put Products")
+app.get("/api/products/:pid", (req, res)=> {
+    const id = req.params.pid
+    const readJson = newManager.getProductByID(id)
+    readJson
+        .then((data) =>{
+            res.status(200)
+            res.send(data)
+        })
+        .catch((data)=> {
+            res.status(500)
+            res.send(data)
+        })
+})
+
+app.put("/api/products/:pid",(req, res)=> { //ID dinámico
+    const id = req.params.pid
+
+    
     res.status(201)
     res.send("My first Put")
 
