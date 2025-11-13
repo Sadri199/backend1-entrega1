e0609fcfd6ca6af3be64072d5e48549b54f2b146
Sadri199 <adrsa_15@hotmail.com>
2025-11-13 16:05:10 -0300

    home handlebars listo, falta implementar websocket

--- controllers/home.controllers.js
+++ controllers/home.controllers.js
@@ -1,3 +1,14 @@
+import { ProductManager } from "../managers/productManager.js"
+
 export const getHome = (req, res) => {
-    res.render("home")
+    const newManager = new ProductManager()
+    const getProduct = newManager.getProduct()
+    getProduct
+        .then((data)=>{
+            console.log("entre acá")
+            const allProducts = data
+            res.render("home", allProducts)
+        }).catch((err)=>{
+            console.error(err)
+        })
 }
